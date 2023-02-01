972785c2333124d8e53742acac32299af3251d54
ThaiQuiNam <117506407+ThaiQuiNam@users.noreply.github.com>
2023-02-01 16:04:43 +0700

    Add files via upload

---
+++ bai4.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+typedef struct
+{
+	char Ma[10];
+	char HoTen[40];
+}SINHVIEN;
+///--------
+void ReadFile(char *FileName);
+void WriteFile(char *FileName);
+void Search(char *FileName);
+
+int main()
+{
+	int c;
+	for(;;)
+	{
+		printf("\n \t*_*_*_*_*_*_*_*_*_*_*_*_*_\n");
+		
+	}
+}
